a90fe307f69f3b39b20ca842eb25e1774f59de86
Cole Albrecht <contact@colealbrecht.ca>
2019-10-19 15:14:15 -0600

    cleaned up mainwindow.py, added comments and fixed the window size.

--- mainwindow.py
+++ mainwindow.py
@@ -3,55 +3,69 @@ from DDListWidget import DDListWidget
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
+        
+        # Main Window
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(483, 244)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
+        MainWindow.resize(485, 245)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(1)
         sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
         MainWindow.setSizePolicy(sizePolicy)
-        MainWindow.setMinimumSize(QtCore.QSize(483, 244))
+        MainWindow.setFixedSize(QtCore.QSize(525, 275))
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("pdfmerge-icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
         
+        # Central Widget
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setEnabled(True)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(1)
         sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
         self.centralwidget.setSizePolicy(sizePolicy)
         self.centralwidget.setObjectName("centralwidget")
-        self.layoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.layoutWidget.setGeometry(QtCore.QRect(380, 10, 92, 201))
-        self.layoutWidget.setObjectName("layoutWidget")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.layoutWidget)
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        
+        # <Grid Layout>
+        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(5, 5, 515, 240))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        
+        # <Vertical Layout>
+        self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
-        self.btnAdd = QtWidgets.QPushButton(self.layoutWidget)
+        
+        # Add Button
+        self.btnAdd = QtWidgets.QPushButton(self.gridLayoutWidget)
         self.btnAdd.setObjectName("btnAdd")
         self.verticalLayout.addWidget(self.btnAdd)
-        self.btnRemove = QtWidgets.QPushButton(self.layoutWidget)
+        
+        # Remove Button
+        self.btnRemove = QtWidgets.QPushButton(self.gridLayoutWidget)
         self.btnRemove.setObjectName("btnRemove")
         self.verticalLayout.addWidget(self.btnRemove)
+        
+        # Spacer between Add/Remove buttons and Combine button
         spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.verticalLayout.addItem(spacerItem)
-        self.btnCombine = QtWidgets.QPushButton(self.layoutWidget)
+        self.btnCombine = QtWidgets.QPushButton(self.gridLayoutWidget)
         self.btnCombine.setObjectName("btnCombine")
         self.verticalLayout.addWidget(self.btnCombine)
-        
+        self.gridLayout.addLayout(self.verticalLayout, 0, 1, 1, 1)
+        # </VerticalLayout>
 
-        self.lstFiles = DDListWidget(self.centralwidget)
-        self.lstFiles.setGeometry(QtCore.QRect(10, 10, 361, 201))
-        
-
+        # File ListWidget
+        self.lstFiles = DDListWidget(self.gridLayoutWidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(1)
         sizePolicy.setHeightForWidth(self.lstFiles.sizePolicy().hasHeightForWidth())
         self.lstFiles.setSizePolicy(sizePolicy)
-        self.lstFiles.setAcceptDrops(False)
+        self.lstFiles.setAcceptDrops(True)
         self.lstFiles.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.lstFiles.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.lstFiles.setFrameShadow(QtWidgets.QFrame.Sunken)
@@ -61,6 +75,11 @@ class Ui_MainWindow(object):
         self.lstFiles.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
         self.lstFiles.setResizeMode(QtWidgets.QListView.Fixed)
         self.lstFiles.setObjectName("lstFiles")
+        self.gridLayout.addWidget(self.lstFiles, 0, 0, 1, 1)
+
+        # </Grid Layout>
+
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setSizeGripEnabled(False)
@@ -69,6 +88,9 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        MainWindow.setTabOrder(self.lstFiles, self.btnAdd)
+        MainWindow.setTabOrder(self.btnAdd, self.btnRemove)
+        MainWindow.setTabOrder(self.btnRemove, self.btnCombine)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
