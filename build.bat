ec5acfe23523a1a87f2211d6f1f6b6660d738573
Cole Albrecht <contact@colealbrecht.ca>
2021-05-19 19:04:34 -0600

    Changed build.bat to build pdfmerge in a single directory instead of a single file and fixed icon loading

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 
-pyinstaller.exe --windowed --onefile --hidden-import pikepdf._cpphelpers -i ./pdfmerge-icon.ico --add-data "./pdfmerge-icon.png;." pdfmerge.py
+pyinstaller.exe --windowed --onedir --hidden-import pikepdf._cpphelpers -i pdfmerge-icon.ico --add-data "pdfmerge-icon.png;." pdfmerge.py
